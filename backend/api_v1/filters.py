753970465c07cdfd964f06a84c60c7e24614a081
Angelina Myagkova <aa.myagkova@gmail.com>
2021-08-01 16:44:39 +0200

    refactoring

--- backend/api_v1/filters.py
+++ backend/api_v1/filters.py
@@ -1,12 +1,11 @@
 import django_filters
-from django_filters import CharFilter
 
 from .models import Ingredient, Recipe, TagsInRecipe
 
 
 class RecipeFilter(django_filters.FilterSet):
-    is_favorited = CharFilter(method='is_favorited_filter')
-    tags = CharFilter(method='tags_filter')
+    is_favorited = django_filters.CharFilter(method='is_favorited_filter')
+    tags = django_filters.CharFilter(method='tags_filter')
 
     class Meta:
         model = Recipe
@@ -16,6 +15,7 @@ class RecipeFilter(django_filters.FilterSet):
         is_favorited = self.request.query_params.get('is_favorited')
         if is_favorited and self.request.user.is_authenticated:
             return queryset.filter(is_favorited__user=self.request.user)
+        return Recipe.objects.none()
 
     def tags_filter(self, queryset, name, value):
         tags = self.request.query_params.getlist('tags')
@@ -23,10 +23,11 @@ class RecipeFilter(django_filters.FilterSet):
             recipes_with_tags = TagsInRecipe.objects.filter(
                 tag__slug__in=tags).values_list('recipe', flat=True)
             return queryset.filter(id__in=recipes_with_tags)
+        return queryset
 
 
 class IngredientFilter(django_filters.FilterSet):
-    name = CharFilter(method='ingredients_filter')
+    name = django_filters.CharFilter(method='ingredients_filter')
 
     class Meta:
         model = Ingredient
@@ -36,3 +37,4 @@ class IngredientFilter(django_filters.FilterSet):
         name = self.request.query_params.get('name')
         if name is not None:
             return queryset.filter(name__icontains=name)
+        return queryset

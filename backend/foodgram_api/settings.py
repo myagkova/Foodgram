1444963acdcd25990416f0835472e7517dcd58bb
Angelina Myagkova <aa.myagkova@gmail.com>
2021-08-06 15:11:37 +0200

    fix paginator

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'fake key for unit test')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 6
 }
 
 SIMPLE_JWT = {

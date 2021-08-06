9d361c44f5a2ddda587f140000bd7036b7e70fb6
Angelina Myagkova <aa.myagkova@gmail.com>
2021-08-06 18:48:03 +0200

    add admin models

--- backend/api_v1/admin.py
+++ backend/api_v1/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import CustomUser, Ingredient, Recipe, Tag
+from .models import CustomUser, Ingredient, IngredientInRecipe, FavoriteRecipe, \
+    Follow, Recipe, ShoppingCart, Tag, TagsInRecipe
 
 
 @admin.register(CustomUser)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('username', 'email')
 
 
 @admin.register(Tag)
@@ -15,9 +16,38 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('name', 'author', 'tags')
+
+    @admin.display(empty_value='0')
+    def view_is_favorited(self, obj):
+        return obj.is_favorited
 
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
+    list_filter = ('name')
+
+
+@admin.register(IngredientInRecipe)
+class IngredientInRecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TagsInRecipe)
+class TagsInRecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FavoriteRecipe)
+class FavoriteRecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
     pass
